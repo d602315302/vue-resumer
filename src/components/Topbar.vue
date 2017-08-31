162f8798bda564a8dd16fe6b792b2b470277cbc1
gavin <602315302@qq.com>
2017-08-31 20:00:06 +0800

    实现导航功能和表单

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,36 +1,55 @@
 <template>
-  <div id="topbar">
-    <div class="wrapper">
-      <span class="logo">Resumer</span>
+    <div id="topbar">
+        <div class="wrapper">
+            <span class="logo">Resumer</span>
+            <div class="actions">
+                <button class="primary">保存</button>
+                <button>预览</button>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'Topbar'
+    name: 'Topbar'
 }
 </script>
 
-<style>
-  #topbar{
-    background:#ffffff;
-    box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
-  }
-  #topbar>.wrapper{
-    min-width: 1024px;
-    max-width: 1440px;
-    margin: 0 auto;
-    height:64px;
-  }
-  #topbar>.wrapper{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 16px;
-  }
-  #topbar .logo{
-    font-size:24px;
-    color:#000000;
-  }
+<style scoped lang="scss">
+#topbar {
+    background: #ffffff;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+    >.wrapper {
+        min-width: 1024px;
+        max-width: 1440px;
+        margin: 0 auto;
+        height: 64px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 0 16px;
+    }
+    .logo {
+        font-size: 24px;
+        color: #000000;
+    }
+    button {
+        width: 72px;
+        height: 32px;
+        border: none;
+        border-radius: 6px;
+        cursor: pointer;
+        font-size: 18px;
+        background: #dddddd;
+        color:#222;
+        &:hover{
+            box-shadow:1px 1px 1px hsla(0,0,0,0.50);
+        }
+        &.primary{
+            background: #02af5f;
+            color:white;
+        }
+    }
+}
 </style>
